152cea05cf88947a3fce5ffbcee26fc716782a66
João Victor <santosjoao.dev@gmail.com>
2025-04-24 14:16:31 -0300

    feat: add pstgres

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+    err := godotenv.Load()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Erro ao carregar o arquivo .env")
+    }
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to dabase: %v\n", err)
+	}
+	defer conn.Close(context.Background())
+
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello")
 	})
